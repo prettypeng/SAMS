95cf86df2116c13a9f4ab5d321e3e0697dccbe54
prettypeng <599248019@qq.com>
2015-07-16 18:34:57 +0800

    Initial Commit

---
+++ SAMS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SAMS
+//
+//  Created by Jack Peng on 15/7/16.
+//  Copyright (c) 2015年 Jack Peng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
